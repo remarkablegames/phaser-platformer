18ff1c69757856c09b7b238ebbd67ec77d079bfe
Mark <mark@remarkablemark.org>
2022-01-26 01:44:07 -0500

    build: replace `@descriptive/web-scripts` with `parcel`

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
-import './index.css';
+import './style.css';
 
 import { Game, Scale } from 'phaser';
 
 import { Boot, Main } from './scenes';
+
+const isProduction = process.env.NODE_ENV === 'production';
 
 /**
  * https://photonstorm.github.io/phaser3-docs/Phaser.Types.Core.html#.GameConfig
@@ -12,8 +14,7 @@ const config = {
   height: 768,
   title: 'Phaser Platformer',
   url: 'https://remarkablegames.org/phaser-platformer/',
-  // see `.env` and `package.json`
-  version: process.env.WEB_APP_VERSION,
+  version: process.env.VERSION,
   scene: [Boot, Main],
   physics: {
     default: 'arcade',
@@ -21,10 +22,10 @@ const config = {
       gravity: {
         y: 400,
       },
-      debug: process.env.NODE_ENV === 'development',
+      debug: !isProduction,
     },
   },
-  disableContextMenu: true,
+  disableContextMenu: isProduction,
   backgroundColor: '#fff',
   scale: {
     mode: Scale.FIT,
